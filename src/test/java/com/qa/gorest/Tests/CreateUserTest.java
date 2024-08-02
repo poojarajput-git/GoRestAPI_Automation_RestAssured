662bd2a5d806201966c32af823260d367c61873d
Pooja Rajput <poojarajput@Poojas-MacBook-Air.local>
2024-08-02 17:15:10 +0530

    Added data provider for creating multiple users

--- src/test/java/com/qa/gorest/Tests/CreateUserTest.java
+++ src/test/java/com/qa/gorest/Tests/CreateUserTest.java
@@ -2,21 +2,33 @@ package com.qa.gorest.Tests;
 
 import com.qa.gorest.Base.BaseTest;
 import com.qa.gorest.CONSTANTS.APIHttpStatus;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-import com.qa.gorest.Client.RestClient;
 import com.qa.gorest.Pojo.UserPOJO;
 import com.qa.gorest.Utils.StringUtils;
+
+import java.util.Iterator;
 
 import static org.hamcrest.Matchers.*;
 
 public class CreateUserTest extends BaseTest {
 	UserPOJO user_pojo;
 
-	@Test
-	public void createUserAllTest() {
+	@DataProvider
+	public Object[][] getUserData(){
+		return new Object[][]{
+			{"Subodh","male","active"},
+			{"Sema","female","inactive"},
+			{"Madhuri","female","active"}
+		};
+
+	}
+
+	@Test(dataProvider = "getUserData")
+	public void createUserAllTest(String name, String gender, String status) {
 		//post
 		user_pojo =
-				new UserPOJO("Arjun KOHLI", StringUtils.generateRandomEmailID(), "male", "active");
+				new UserPOJO(name, StringUtils.generateRandomEmailID(), gender, status);
 		Integer userID = restClient.post(GOREST_ENDPOINT, "JSON", user_pojo, true, true)
 		          .then().assertThat().statusCode(APIHttpStatus.CREATED_201.getCode())
 		          .extract().path("id");
